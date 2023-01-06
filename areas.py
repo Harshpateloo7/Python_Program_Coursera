e7db7a5ce4f30c62471c256f4a0b4efe99995d7d
Harshadkumar Patel <64983530+Harshpateloo7@users.noreply.github.com>
2023-01-05 21:15:57 -0500

    Update areas.py

--- areas.py
+++ areas.py
@@ -5,3 +5,5 @@ def rectangle(base, height):
     return base * height
 def circle(radius):
     return math.pi(radius**2)
+def donut(outside_radius, inside_radius):
+    return circle(outside_radius) - circle(inside_radius)
